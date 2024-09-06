5f766522912d1131237bc67ce5a0d8215b77f167
Jeison-R <jedaremo@gmail.com>
2024-09-06 14:03:53 -0500

    Arreglos en actions menu

--- client/src/components/experience-table/ActionsMenu.tsx
+++ client/src/components/experience-table/ActionsMenu.tsx
@@ -1,27 +1,38 @@
 'use client'
 
+import type { Payment } from './experience-table'
+
 import { useState } from 'react'
 import { Eye, Pencil, Trash, MoreVertical } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 
-export function ActionsMenu() {
+interface ActionsMenuProps {
+  row: {
+    original: Payment
+  }
+}
+
+export function ActionsMenu({ row }: ActionsMenuProps) {
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
 
-  const handleEdit = () => {
-    // Lógica para editar el registro
+  const handleEdit = (data: Payment) => {
+    return data
+    // Aquí puedes agregar la lógica para editar el registro
   }
 
-  const handleDelete = () => {
-    // Lógica para eliminar el registro
+  const handleDelete = (data: Payment) => {
+    return data
+    // Aquí puedes agregar la lógica para eliminar el registro
   }
 
-  const handleView = () => {
-    // Lógica para ver el registro
+  const handleView = (data: Payment) => {
+    return data
+    // Aquí puedes agregar la lógica para ver el registro
   }
 
   return (
@@ -36,7 +47,7 @@ export function ActionsMenu() {
               className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
               type="button"
               onClick={() => {
-                handleView()
+                handleView(row.original)
                 setIsOpen(false)
               }}
             >
@@ -47,7 +58,7 @@ export function ActionsMenu() {
               className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
               type="button"
               onClick={() => {
-                handleEdit()
+                handleEdit(row.original)
                 setIsOpen(false)
               }}
             >
@@ -58,7 +69,7 @@ export function ActionsMenu() {
               className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
               type="button"
               onClick={() => {
-                handleDelete()
+                handleDelete(row.original)
                 setIsOpen(false)
               }}
             >
