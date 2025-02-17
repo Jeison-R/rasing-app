6e8e03a83f118b86eda7264fd4931531e33e275f
Jeison-R <jedaremo@gmail.com>
2025-02-17 08:57:21 -0500

    cambio de logo

--- client/src/components/commons/main-nav.tsx
+++ client/src/components/commons/main-nav.tsx
@@ -7,6 +7,8 @@ import { cn } from '@/lib/utils'
 import { Logo } from '@/assets/icons/logo'
 import { ROUTES } from '@/consts/routes'
 
+import { RasingCabe } from '../../assets/icons/rasing-Cabe'
+
 export function MainNav() {
   const pathname = usePathname()
 
@@ -14,7 +16,7 @@ export function MainNav() {
     <div className="hidden w-full flex-col justify-between gap-6 py-2 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
       <Link className="flex items-center gap-4" href="/">
         <Logo width={40} />
-        <span className="text-xl font-bold">Rasing S.A.S</span>
+        <RasingCabe width={150} />
       </Link>
       <nav className="flex items-center justify-end gap-4 text-sm lg:gap-6">
         <Link className={cn('transition-colors hover:text-foreground/80', pathname.startsWith(ROUTES.EXPERIENCES) ? 'text-foreground' : 'text-foreground/60')} href={ROUTES.EXPERIENCES}>
