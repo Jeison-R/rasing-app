442e0c5356d13c3fc9b2591688b0b0f57e1ab1a2
Jeison-R <jedaremo@gmail.com>
2025-02-19 10:43:26 -0500

    cmabio de color en el logo modo oscuro

--- client/src/components/commons/main-nav.tsx
+++ client/src/components/commons/main-nav.tsx
@@ -2,21 +2,41 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 import { cn } from '@/lib/utils'
 import { Logo } from '@/assets/icons/logo'
 import { ROUTES } from '@/consts/routes'
 
+import { RasingCabeDark } from '../../assets/icons/rasing-CabeDark'
 import { RasingCabe } from '../../assets/icons/rasing-Cabe'
 
 export function MainNav() {
   const pathname = usePathname()
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    const updateTheme = () => {
+      setIsDark(document.documentElement.classList.contains('dark'))
+    }
+
+    updateTheme() // Verificar tema al montar el componente
+
+    // Escuchar cambios en el tema
+    const observer = new MutationObserver(updateTheme)
+
+    observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] })
+
+    return () => {
+      observer.disconnect()
+    } // Limpiar el observer al desmontar
+  }, [])
 
   return (
     <div className="hidden w-full flex-col justify-between gap-6 py-2 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
       <Link className="flex items-center gap-4" href="/">
         <Logo width={40} />
-        <RasingCabe width={150} />
+        {isDark ? <RasingCabeDark width={150} /> : <RasingCabe width={150} />}
       </Link>
       <nav className="flex items-center justify-end gap-4 text-sm lg:gap-6">
         <Link className={cn('transition-colors hover:text-foreground/80', pathname.startsWith(ROUTES.EXPERIENCES) ? 'text-foreground' : 'text-foreground/60')} href={ROUTES.EXPERIENCES}>
